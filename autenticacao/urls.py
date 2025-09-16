46d90e80200d2ddda490544d4399ea01b2c78186
Gustavo Lopes <guhrlopes999@gmail.com>
2025-09-16 11:59:46 -0300

    refactor: Alterado path de URLS

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -4,5 +4,5 @@ from autenticacao.views import login, singup, members
 urlpatterns = [
   path('', login, name='login'),
   path('cadastro', singup, name='singup'),
-  path('members/', members, name='members')
+  path('members/', members, name='members'),
 ]
